1f06e63ebfe4c15cac6a8abcf4cc4211492b6ea5
dimitar <mitko.box.hs@gmail.com>
2020-04-05 19:08:58 +0300

    Fixed class name and methods

--- treasures.py
+++ treasures.py
@@ -4,16 +4,16 @@ from weapon import Weapon
 from spell import Spell
 
 
-class Treasures:
+class Treasure:
 
     def __init__(self):
         dict_of_treasures = {
-            "weapons": Weapon(name='--Sword--', damage=40),
-            "weapons": Weapon(name='--Fighting knives--', damage=20),
-            "weapons": Weapon(name='--Daggers--', damage=40),
-            "spells": Spell(name='--Freezing spell--', damage=30, mana_cost=50, cast_range=1),
-            "spells": Spell(name='--Black spell--', damage=30, mana_cost=50, cast_range=1),
-            "spells": Spell(name='--Disappear spell--', damage=30, mana_cost=50, cast_range=1),
+            "weapon": Weapon(name='--Sword--', damage=40),
+            "weapon": Weapon(name='--Fighting knives--', damage=20),
+            "weapon": Weapon(name='--Daggers--', damage=40),
+            "spell": Spell(name='--Freezing spell--', damage=30, mana_cost=50, cast_range=1),
+            "spell": Spell(name='--Black spell--', damage=30, mana_cost=50, cast_range=1),
+            "spell": Spell(name='--Disappear spell--', damage=30, mana_cost=50, cast_range=1),
             "mana": 10,
             "mana": 15,
             "mana": 20,
@@ -33,21 +33,22 @@ class Treasures:
 
     def pick_treasure(self):
         if self.kind_of_treasure == 'mana':
-            print("Hero has found a mana!")
+            print("It is a mana potion!")
             return self.treasure
 
         elif self.kind_of_treasure == 'health':
-            print('Hero has found a health treasure!')
+            print('It is a health potion!')
             return self.treasure
 
-        elif self.kind_of_treasure == 'weapons':
-            print("Hero has found a weapon!")
+        elif self.kind_of_treasure == 'weapon':
+            print("It is a weapon!")
             return self.treasure
 
-        elif self.kind_of_treasure == 'spells':
-            print("Hero has learned a spell!")
+        elif self.kind_of_treasure == 'spell':
+            print("It is a spell!")
             return self.treasure
 
-
-treasure = Treasures()
+'''
+treasure = Treasure()
 print(treasure.pick_treasure())
+'''
